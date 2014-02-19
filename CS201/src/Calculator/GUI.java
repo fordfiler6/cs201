4b45484a0bc0eb3877e40899b448e95e583ca196
Ford <Ford@Ford-PC>
2014-02-19 14:39:21 -0800

    Added Input and Equation Label to Gui

--- CS201/src/Calculator/GUI.java
+++ CS201/src/Calculator/GUI.java
@@ -1,11 +1,16 @@
 package Calculator;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -13,13 +18,25 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
 
 public class GUI extends JFrame 
 {
+	private static final int APP_WIDTH = (int) (Toolkit.getDefaultToolkit().getScreenSize().getWidth() * (515.0/1920.0));;
+	private static final int APP_HEIGHT = (int) (Toolkit.getDefaultToolkit().getScreenSize().getHeight()* (391.0/1080.0));
+	private static final int X_PADDING = 10;
+	private static final int Y_PADDING = 20;
+	private static final int INNER_PANEL_WIDTH = APP_WIDTH-(X_PADDING*3);
+	private static final int TEXT_PANEL_HEIGHT = 150;
+	private static final int INPUT_TEXT_HEIGHT = 100;
+	private static final int EQUATION_HEIGHT = TEXT_PANEL_HEIGHT - INPUT_TEXT_HEIGHT;
+
 	JLabel debug;
 	GUI()
 	{
-		setSize(515,391);
+		Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		setSize(APP_WIDTH,APP_HEIGHT);
 		setResizable(false);
 		JMenuBar menu = generateMenuBar();
 		this.setJMenuBar(menu);
@@ -41,8 +58,45 @@ public class GUI extends JFrame
 	private JPanel generateMainPanel()
 	{
 		JPanel main = new JPanel();
-		main.setLayout(new GridBagLayout());
+		main.setLayout(null);
+		
+
+		
+		
+		
+		main.add(generateTextDisplay());
+		
+		
 		return main;
+	}
+	private JPanel generateTextDisplay()
+	{
+		JPanel textDisplay = new JPanel();
+		textDisplay.setSize(INNER_PANEL_WIDTH, TEXT_PANEL_HEIGHT);
+		textDisplay.setLocation(X_PADDING, Y_PADDING);
+		textDisplay.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		textDisplay.setLayout(new BorderLayout());
+		
+		JLabel textInput = new JLabel();
+		textInput.setSize(INNER_PANEL_WIDTH, INPUT_TEXT_HEIGHT);
+		textInput.setHorizontalAlignment(SwingConstants.RIGHT);
+		textInput.setFont(new Font(textInput.getFont().getName(), Font.PLAIN, INPUT_TEXT_HEIGHT));
+		
+		textDisplay.add(textInput, BorderLayout.PAGE_END);
+		textInput.setText("Inputs");
+	
+		JLabel equationDisplay = new JLabel();
+		equationDisplay.setSize(INNER_PANEL_WIDTH, EQUATION_HEIGHT);
+		equationDisplay.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		equationDisplay.setFont(new Font(equationDisplay.getFont().getName(), Font.PLAIN, EQUATION_HEIGHT));
+
+		equationDisplay.setText("Equation");
+		textDisplay.add(equationDisplay, BorderLayout.PAGE_START);
+		
+		
+		return textDisplay;
 	}
 	private JMenuBar generateMenuBar()
 	{
